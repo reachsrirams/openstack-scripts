938e99367de717f228a7dc1e99fc1377fe245b30
Sriram S <sriramsubramanian@bitbucket.org>
2015-01-12 10:05:37 +0530

    Update Compute and Controller Install scripts. Added new passwords in the parameters file.

--- install-parameters.sh
+++ install-parameters.sh
@@ -7,4 +7,10 @@ readonly network_node_ip_address="192.168.100.101"
 readonly mysql_user="root"
 readonly mysql_password="password"
 
+readonly rabbitmq_user="guest"
 readonly rabbitmq_password="password"
+
+readonly keystone_db_password="password"
+readonly glance_db_password="password"
+
+readonly admin_tenant_password="password"
