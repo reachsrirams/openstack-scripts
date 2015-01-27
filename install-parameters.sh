b49718713c654aebbf4dafb78cfde7452d20379f
Sriram S <sriramsubramanian@bitbucket.org>
2015-01-27 23:06:28 +0530

    Updated to use Controller IP instead of Hostname. Fixed error in a debug statement.

--- install-parameters.sh
+++ install-parameters.sh
@@ -1,7 +1,7 @@
 readonly sleep_duration=5
-readonly controller_host_name="controller"
+controller_host_name="controller"
 controller_ip_address="192.168.100.100"
-readonly network_node_host_name="networknode"
+network_node_host_name="networknode"
 network_node_ip_address="192.168.100.101"
 
 readonly mysql_user="root"
