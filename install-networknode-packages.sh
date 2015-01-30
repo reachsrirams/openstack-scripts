23e9944c6534f4949fa7e481991650f6249c9f0f
Sriram S <sriramsubramanian@bitbucket.org>
2015-01-30 08:49:22 +0530

    Moving few scripts to util directory. Adding scripts for Network Node.

---
+++ install-networknode-packages.sh
@@ -0,0 +1,10 @@
+source install-parameters.sh
+
+echo "About to install Nova for Compute"
+apt-get install nova-compute sysfsutils -y
+
+echo "About to install Neutron for Compute"
+apt-get install neutron-plugin-ml2 neutron-plugin-openvswitch-agent -y
+
+echo "About to install Ceilometer for Compute"
+apt-get install ceilometer-agent-compute -y
