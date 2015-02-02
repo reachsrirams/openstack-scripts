8da473bdade80f1cef6c0a067bf0c33dc41f2a80
Sriram S <sriramsubramanian@bitbucket.org>
2015-02-02 16:12:49 +0530

    Fixed script errors for network node

--- configure-networknode-packages.sh
+++ configure-networknode-packages.sh
@@ -9,4 +9,4 @@ echo_and_sleep "About to configure sysctl" 2
 bash networknode/configure-kernel-parameters.sh
 
 echo_and_sleep "About to configure Neutron for Network Node" 2
-bash networknode/configure-neutron-compute.sh $controller_host_name $rabbitmq_password $neutron_password $1 $2
+bash networknode/configure-neutron-networknode.sh $controller_host_name $rabbitmq_password $neutron_password $1 $2
