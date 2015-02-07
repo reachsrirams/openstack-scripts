4aa604dd3d232f1948e732a58fcce52633cf79f4
Sriram S <sriramsubramanian@bitbucket.org>
2015-02-07 23:26:17 +0530

    Updating etc-hosts scripts to take eth0 IP automatically

--- update-compute-etc-hosts.sh
+++ update-compute-etc-hosts.sh
@@ -1,12 +1,22 @@
 source install-parameters.sh
-if [ $# -lt 3 ]
+if [ $# -lt 1 ]
 	then
-		echo "Correct syntax: $0 <compute-node-name> <compute-node-ip> <controller_ip>"
+		echo "Correct syntax: $0 <controller-ip> <compute-host-name>"
 		exit 1;
 fi
-echo "IP to be configured in /etc/hosts: "$2
-bash change-ip-in-etc-hosts.sh $1 $2
+compute_host_name="compute1"
+if [ $# -eq 2 ]
+	then
+		compute_host_name=$2
+fi
+compute_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+echo "eth0 IP is: $compute_ip_address"
+echo "Compute host name: $compute_host_name"
+sleep 3
 
-echo "Adding controller node info to /etc/hosts"
+echo "Updating compute IP address to /etc/hosts"
+bash change-ip-in-etc-hosts.sh $compute_host_name $compute_ip_address
+
+echo "Updating controller IP address to /etc/hosts"
 bash change-ip-in-etc-hosts.sh $controller_host_name $3
 
--- update-networknode-etc-hosts.sh
+++ update-networknode-etc-hosts.sh
@@ -1,12 +1,16 @@
-source install-parameters.sh
-if [ $# -lt 2 ]
-	then
-		echo "Correct syntax: $0 <networknode-ip> <controller-ip>"
-		exit 1;
+ource install-parameters.sh
+if [ $# -lt 1 ]
+        then
+                echo "Correct syntax: $0 <controller-ip>"
+                exit 1;
 fi
-echo "Adding network node info to /etc/hosts"
-bash change-ip-in-etc-hosts.sh $network_node_host_name $1
 
-echo "Adding controller node info to /etc/hosts"
-bash change-ip-in-etc-hosts.sh $controller_host_name $2
+netnode_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+echo "eth0 IP is: $netnode_ip_address"
+sleep 3
 
+echo "Updating network node IP address to /etc/hosts"
+bash change-ip-in-etc-hosts.sh $network_node_host_name $netnode_ip_address
+
+echo "Updating controller IP address to /etc/hosts"
+bash change-ip-in-etc-hosts.sh $controller_host_name $1
