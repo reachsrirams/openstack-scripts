deb3e63cc4eb23fb2d91407498176ea44998a69d
Sriram S <sriramsubramanian@bitbucket.org>
2015-02-11 17:44:09 +0530

    Compute hostname is auto-detected

--- update-compute-etc-hosts.sh
+++ update-compute-etc-hosts.sh
@@ -1,14 +1,10 @@
 source install-parameters.sh
 if [ $# -lt 1 ]
 	then
-		echo "Correct syntax: $0 <controller-ip> <compute-host-name>"
+		echo "Correct syntax: $0 <controller-ip>"
 		exit 1;
 fi
-compute_host_name="compute1"
-if [ $# -eq 2 ]
-	then
-		compute_host_name=$2
-fi
+compute_host_name=`hostname`
 compute_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "eth0 IP is: $compute_ip_address"
 echo "Compute host name: $compute_host_name"
