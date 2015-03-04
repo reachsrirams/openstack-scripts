09417e38e8710d3ba0902909efd3be422343c031
Sriram S <sriramsubramanian@bitbucket.org>
2015-03-05 01:15:20 +0530

    updated and renamed the remove packages script

--- remove.sh
+++ remove.sh
@@ -13,12 +13,24 @@ if [ "$1" == "controller" ]
 		apt-get purge nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient -y
 		apt-get purge neutron-server neutron-plugin-ml2 python-neutronclient -y
 		apt-get purge openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache -y
+		apt-get purge mongodb-server -y
+		apt-get purge ceilometer-api ceilometer-collector ceilometer-agent-central ceilometer-agent-notification ceilometer-alarm-evaluator ceilometer-alarm-notifier python-ceilometerclient -y
 		apt-get autoremove -y
-fi
-
-if [ "$1" == "compute" ]
+elif [ "$1" == "compute" ]
 	then
+		echo "About to remove packages for Compute Node"
+		sleep 2
 		apt-get purge nova-compute sysfsutils -y
 		apt-get purge neutron-plugin-ml2 neutron-plugin-openvswitch-agent -y
+		apt-get purge ceilometer-agent-compute -y
 		apt-get autoremove -y
+elif [ "$1" == "networknode" ]
+	then
+		echo "About to remove packages for Network Node"
+		sleep 2
+		apt-get purge neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-plugin-linuxbridge-agent -y
+		apt-get autoremove -y
+else
+	echo "Correct Syntax: $0 [ controller | compute | networknode ]"
+	exit 1;
 fi
