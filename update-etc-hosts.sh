099c0bb9fee21839e17538f9d237cd5d341f120f
Sriram S <sriramsubramanian@bitbucket.org>
2015-03-04 14:40:23 +0530

    moved underlying scripts to lib

--- update-etc-hosts.sh
+++ update-etc-hosts.sh
@@ -1,4 +1,4 @@
-source install-parameters.sh
+source config-parameters.sh
 
 local_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "eth0 IP is: $local_ip_address"
@@ -11,16 +11,16 @@ if [ "$1" == "controller" ]
 	then
 		controller_ip_address=$local_ip
 		echo_and_sleep "Adding controller node info to /etc/hosts"
-		bash change-ip-in-etc-hosts.sh $controller_host_name $controller_ip_address
+		bash lib/change-ip-in-etc-hosts.sh $controller_host_name $controller_ip_address
 elif [ "$1" == "compute" ] || [ "$1" == "networknode" ]
 	then
 		if [ $# -eq 2 ]
 			then
 				echo "Updating local node IP address to /etc/hosts"
-				bash change-ip-in-etc-hosts.sh $local_host_name $local_ip_address
+				bash lib/change-ip-in-etc-hosts.sh $local_host_name $local_ip_address
 	
 				echo "Updating controller IP address to /etc/hosts"
-				bash change-ip-in-etc-hosts.sh $controller_host_name $2
+				bash lib/change-ip-in-etc-hosts.sh $controller_host_name $2
 		else
 			echo "Correct syntax: $0 [ compute | networknode ] <controller-ip-address>"
 			exit 1;
