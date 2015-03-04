9481f3387de1fe32597f44a010fc62e067e05004
Sriram S <sriramsubramanian@bitbucket.org>
2015-03-04 14:47:02 +0530

    moved underlying scripts to lib

--- configure-packages.sh
+++ configure-packages.sh
@@ -1,36 +1,36 @@
 source install-parameters.sh
-if [ $# -lt 2 ]
+if [ $# -ne 2 ]
 	then
 		echo "Correct syntax: $0 [ compute | networknode ] <data-plane-interface>"
 		exit 1;
 fi
 
-if [ "$1" == "networknode" ]
+if [ "$1" == "compute" ]
 	then
-		compute_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-		echo "eth0 IP is: $compute_ip_address"
+		local_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+		echo "eth0 IP is: $local_ip_address"
 		sleep 3
 		
 		echo_and_sleep "About to configure forwarding" 2
-		bash configure-forwarding.sh compute
+		bash lib/configure-forwarding.sh compute
 
 		echo_and_sleep "About to configure Nova for Compute" 3
-		bash configure-nova-compute.sh $controller_host_name $nova_password $rabbitmq_password $compute_ip_address
+		bash configure-nova-compute.sh $controller_host_name $nova_password $rabbitmq_password $local_ip_address
 		
 		echo_and_sleep "About to configure Neutron for Compute" 3
-		bash configure-neutron-compute.sh $controller_host_name $rabbitmq_password $neutron_password $1
+		bash configure-neutron-compute.sh $controller_host_name $rabbitmq_password $neutron_password $2
 		
 		metering_secret="password"
 		echo_and_sleep "About to configure Ceilometer for Compute" 3
 		bash configure-ceilometer-compute.sh $controller_host_name $rabbitmq_password $neutron_password $metering_secret
-fi
-
-
-if [ "$1" == "networknode" ]
+elif [ "$1" == "networknode" ]
 	then
 		echo_and_sleep "About to configure forwarding" 2
 		bash configure-forwarding.sh networknode
 
 		echo_and_sleep "About to configure Neutron for Network Node" 2
 		bash configure-neutron-networknode.sh $controller_host_name $rabbitmq_password $neutron_password $2
+else
+	echo "Correct syntax: $0 [ compute | networknode ] <data-plane-interface>"
+	exit 1;
 fi
