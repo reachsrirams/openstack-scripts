ef1f8cc96721b82a89289a280d5b02585346d9ac
Sriram S <sriramsubramanian@bitbucket.org>
2015-03-06 11:21:18 +0530

    consolidated install scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,79 @@
+function install-common-packages() {
+	echo "About to install NTP Server"
+	sleep 5
+	apt-get install ntp -y
+	service ntp restart
+	
+	echo "About to configure Packages for JUNO"
+	sleep 5
+	apt-get install ubuntu-cloud-keyring -y
+	echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu" \
+  	"trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list
+	echo "Doing full system update"
+	sleep 5
+	apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+	apt-get autoremove -y
+}
+
+function install-controller-packages() {
+	echo "Installing MariaDB..."
+	sleep 5
+	apt-get install mariadb-server python-mysqldb -y
+	
+	echo "Installing RabbitMQ..." 
+	sleep 5
+	apt-get install rabbitmq-server -y
+	
+	echo "Installing Keystone..."
+	sleep 5
+	apt-get install keystone python-keystoneclient -y
+	
+	echo "Installing Glance..."
+	sleep 2
+	apt-get install glance python-glanceclient -y
+	
+	echo "Installing Nova for Controller"
+	sleep 2
+	apt-get install nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient -y
+	
+	echo "Installing Neutron for Controller"
+	sleep 2
+	apt-get install neutron-server neutron-plugin-ml2 python-neutronclient -y
+	
+	echo "Installing Horizon..."
+	sleep 2
+	apt-get install openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache -y
+	
+	echo "Installing Ceilometer for Controller"
+	sleep 2
+	apt-get install mongodb-server -y
+	apt-get install ceilometer-api ceilometer-collector ceilometer-agent-central ceilometer-agent-notification ceilometer-alarm-evaluator ceilometer-alarm-notifier python-ceilometerclient -y
+
+	apt-get autoremove -y
+}
+
+function install-compute-packages() {
+	echo "About to install Nova for Compute"
+	apt-get install nova-compute sysfsutils -y
+
+	echo "About to install Neutron for Compute"
+	apt-get install neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-plugin-linuxbridge-agent -y
+	
+	echo "About to install Ceilometer for Compute"
+	apt-get install ceilometer-agent-compute -y
+	
+	apt-get autoremove -y
+}
+
+function install-networknode-packages() {
+	echo "About to install Neutron for Network Node..."
+	sleep 5
+	apt-get install neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-plugin-linuxbridge-agent -y
+	apt-get autoremove -y
+}
+
+
+
 if [ $# -ne 1 ]
 	then
 		echo "Correct Syntax: $0 [ allinone | controller | compute | networknode | controller_networknode | common ]"
@@ -7,25 +83,25 @@ fi
 if [ "$1" == "allinone" ]
 	then
 		echo "Installing packages for All-in-One"
-		bash lib/install-packages.sh common
-		bash lib/install-packages.sh controller
-		bash lib/install-packages.sh compute
-		bash lib/install-packages.sh networknode
+		install-common-packages
+		install-controller-packages
+		install-compute-packages
+		install-network-node-packages
 elif [ "$1" == "controller" ] || [ "$1" == "compute" ] || [ "$1" == "networknode" ]
 	then
 		echo "Installing packages for: "$1
-		bash lib/install-packages.sh common
-		bash lib/install-packages.sh $1
+		install-common-packages
+		install-$1-packages
 elif [ "$1" == "controller_networknode" ]
 	then
 		echo "Installing packages for Controller and Network Node"
-		bash lib/install-packages.sh common
-		bash lib/install-packages.sh controller
-		bash lib/install-packages.sh networknode
+		install-common-packages
+		install-controller-packages
+		install-network-node-packages
 elif [ "$1" == "common" ]
 	then
 		echo "Installing common packages"
-		bash lib/install-packages.sh common
+		install-common-packages
 else
 	
 	echo "Correct Syntax: $0 [ allinone | controller | compute | networknode | controller_networknode | common ]"
