61f2b6a0b20313ad4d4dae61aeae37e36e880a18
Sriram S <sriram@innervoice.in>
2015-03-09 14:49:14 +0530

    Configure packages script - final touch

--- configure.sh
+++ configure.sh
@@ -7,16 +7,19 @@ fi
 if [ "$1" == "allinone" ]
 	then
 		echo "Configuring packages for All-in-one"
+		sleep 5
 		bash lib/configure-packages.sh controller 
 		bash lib/configure-packages.sh networknode
 		bash lib/configure-packages.sh compute 
 elif [ "$1" == "controller" ] || [ "$1" == "compute" ] || [ "$1" == "networknode" ]
 	then
 		echo "Configuring packages for: "$1
+		sleep 5
 		bash lib/configure-packages.sh $1 
 elif [ "$1" == "controller_networknode" ]
 	then
 		echo "Configuring packages for Controller and Network Node"
+		sleep 5
 		bash lib/configure-packages.sh controller 
 		bash lib/configure-packages.sh networknode
 else
