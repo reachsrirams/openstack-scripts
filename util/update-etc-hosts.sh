34d5ebe274d373073bacd442aafd1087553b3e1d
Sriram S <sriram@innervoice.in>
2015-03-12 21:50:55 +0530

    Moved and updated update /etc/hosts and update IP scripts

--- util/update-etc-hosts.sh
+++ util/update-etc-hosts.sh
@@ -1,6 +1,5 @@
 echo "Running: $0 $@"
 dir_path=$(dirname $0)
-source $dir_path/lib/config-parameters.sh
 
 local_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "eth0 IP is: $local_ip_address"
@@ -13,22 +12,22 @@ if [ "$1" == "controller" ]
 	then
 		echo "Adding controller node info to /etc/hosts"
 		sleep 3
-		bash $dir_path/lib/change-ip-in-etc-hosts.sh $controller_host_name $local_ip_address
+		bash $dir_path/change-ip-in-etc-hosts.sh $controller_host_name $local_ip_address
 elif [ "$1" == "compute" ] || [ "$1" == "networknode" ]
 	then
-		if [ $# -eq 2 ]
+		if [ $# -eq 3 ]
 			then
 				echo "Updating local node IP address to /etc/hosts"
-				bash $dir_path/lib/change-ip-in-etc-hosts.sh $local_host_name $local_ip_address
+				bash $dir_path/change-ip-in-etc-hosts.sh $local_host_name $local_ip_address
 	
 				echo "Updating controller IP address to /etc/hosts"
-				bash $dir_path/lib/change-ip-in-etc-hosts.sh $controller_host_name $2
+				bash $dir_path/change-ip-in-etc-hosts.sh $2 $3
 		else
-			echo "Correct syntax: $0 [ compute | networknode ] <controller-ip-address>"
+			echo "Correct syntax: $0 [ compute | networknode ] <controller-host-name> <controller-ip-address>"
 			exit 1;
 		fi
 else
-	echo "Correct syntax: $0 [ controller | compute | networknode ] <controller-ip-address>"
+	echo "Correct syntax: $0 [ controller | compute | networknode ] <controller-host-name> <controller-ip-address>"
 	exit 1;
 fi
 
--- util/update-ip.sh
+++ util/update-ip.sh
@@ -1,22 +1,22 @@
 if [ $# -lt 1 ]
 	then
-		echo "Correct Syntax: $0 [ controller | compute | networknode ] <controller-ip-address>"
+		echo "Correct Syntax: $0 [ controller | compute | networknode ] <controller-host-name> <controller-ip-address>"
 		exit 1;
 fi
 
 case $1 in
 	controller) 
-		bash $(dirname $0)/../update-etc-hosts.sh controller
+		bash $(dirname $0)/update-etc-hosts.sh controller
 		bash $(dirname $0)/update-nova-config-ip.sh controller
 		bash $(dirname $0)/restart.sh controller all
 		;;
 	compute)
-		bash $(dirname $0)/../update-etc-hosts.sh compute $2
-		bash $(dirname $0)/update-nova-config-ip.sh compute $2
+		bash $(dirname $0)/update-etc-hosts.sh compute $2 $3
+		bash $(dirname $0)/update-nova-config-ip.sh compute $3
 		bash $(dirname $0)/restart.sh compute all
 		;;
 	networknode)
-		bash $(dirname $0)/../update-etc-hosts.sh networknode $2
+		bash $(dirname $0)/update-etc-hosts.sh networknode $2 $3
 		bash $(dirname $0)/restart.sh networknode all
 		;;
 	*)
