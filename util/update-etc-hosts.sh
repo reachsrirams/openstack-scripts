8f705a1a7b52f63a3148fc08c10fc82ed93b3c34
Sriram S <sriram@innervoice.in>
2015-03-14 21:50:58 +0530

    Fixed the script to use detected node type

--- util/update-etc-hosts.sh
+++ util/update-etc-hosts.sh
@@ -1,39 +1,41 @@
 echo "Running: $0 $@"
 dir_path=$(dirname $0)
 
+node_type=`bash $(dirname $0)/detect-nodetype.sh`
+echo "Local host type is: $node_type"
+
 local_ip_address=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-echo "eth0 IP is: $local_ip_address"
-sleep 3
+echo "Local host IP is: $local_ip_address"
 
 local_host_name=`hostname`
 echo "Local host name: $local_host_name"
+sleep 3
 
-if [ "$1" == "controller" ]
+if [ "$node_type" == "controller" ]
 	then
-		if [ $# -eq 2 ]
+		if [ $# -eq 1 ]
 			then
 				echo "Adding controller node info to /etc/hosts"
-				sleep 2
-				bash $dir_path/change-ip-in-etc-hosts.sh $2 $local_ip_address
+				bash $dir_path/change-ip-in-etc-hosts.sh $1 $local_ip_address
 		else
-			echo "Correct syntax: $0 controller <controller-host-name>"
+			echo "Correct syntax: $0 <controller-host-name>"
 			exit 1;
 		fi
-elif [ "$1" == "compute" ] || [ "$1" == "networknode" ]
+elif [ "$node_type" == "compute" ] || [ "$node_type" == "networknode" ]
 	then
-		if [ $# -eq 3 ]
+		if [ $# -eq 2 ]
 			then
 				echo "Updating local node IP address to /etc/hosts"
 				bash $dir_path/change-ip-in-etc-hosts.sh $local_host_name $local_ip_address
 	
 				echo "Updating controller IP address to /etc/hosts"
-				bash $dir_path/change-ip-in-etc-hosts.sh $2 $3
+				bash $dir_path/change-ip-in-etc-hosts.sh $1 $2
 		else
-			echo "Correct syntax: $0 [ compute | networknode ] <controller-host-name> <controller-ip-address>"
+			echo "Correct syntax: $0 <controller-host-name> <controller-ip-address>"
 			exit 1;
 		fi
 else
-	echo "Correct syntax: $0 [ controller | compute | networknode ] <controller-host-name> <controller-ip-address>"
+	echo "Unsupported Node type for $0: $node_type"
 	exit 1;
 fi
 
