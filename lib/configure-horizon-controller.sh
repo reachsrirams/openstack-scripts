a871a667e1ab9372657661ca8a9d30a6c548f476
Sriram S <sriram@innervoice.in>
2015-10-12 21:15:33 +0530

    Added role change for Horizon

--- lib/configure-horizon-controller.sh
+++ lib/configure-horizon-controller.sh
@@ -12,9 +12,11 @@ echo "}"
 echo "*********"
 sed -e "/^ALLOWED_HOSTS =.*$/s/^.*$/ALLOWED_HOSTS = ['*']/" -i /etc/openstack-dashboard/local_settings.py
 sed -e "/^OPENSTACK_HOST =.*$/s/^.*$/OPENSTACK_HOST = \""$1"\"/" -i /etc/openstack-dashboard/local_settings.py
+sed -e "/^OPENSTACK_KEYSTONE_DEFAULT_ROLE =.*$/s/^.*$/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"user\"/" -i /etc/openstack-dashboard/local_settings.py
 grep "ALLOWED_HOSTS" /etc/openstack-dashboard/local_settings.py
 grep "OPENSTACK_HOST" /etc/openstack-dashboard/local_settings.py
 echo_and_sleep "Restarting apache2" 3
+service apache2 reload
 service apache2 restart
 echo_and_sleep "Restarting memcached" 3
 service memcached restart
