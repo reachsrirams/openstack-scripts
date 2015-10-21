674d5688a27d6fba3f463b6731c16806800e73ba
Sriram S <sriram@innervoice.in>
2015-10-22 01:08:18 +0530

    script cleanup and consolidation

--- util/update-ip.sh
+++ util/update-ip.sh
@@ -11,15 +11,45 @@ if [ $# -lt 1 ]
 		exit 1;
 fi
 
+
+function update-nova-config-ip() {
+	eth0_ip=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+	echo "Local Node IP: $eth0_ip"
+	sleep 2
+
+	crudini --set /etc/nova/nova.conf DEFAULT my_ip $eth0_ip
+	crudini --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address $eth0_ip
+
+	if [ "$1" == "controller" ]
+		then
+			crudini --set /etc/nova/nova.conf DEFAULT vncserver_listen $eth0_ip
+			sleep 2
+			service nova-novncproxy restart
+		else
+			if [ -z "$2" ]
+				then	
+					echo "Correct syntax for Compute Node: $0 <controller-ip-address>"
+					exit 1;
+				else
+					crudini --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+					crudini --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$2:6080/vnc_auto.html
+					sleep 2
+					service nova-compute restart
+			fi
+	fi
+		
+	echo "Updated Nova Config file for $node_type"
+}
+
 case $node_type in
 	controller) 
 		bash $(dirname $0)/update-etc-hosts.sh $1
-		bash $(dirname $0)/update-nova-config-ip.sh
+		update-nova-config-ip controller
 		bash $(dirname $0)/manage-services.sh all restart
 		;;
 	compute)
 		bash $(dirname $0)/update-etc-hosts.sh $1 $2
-		bash $(dirname $0)/update-nova-config-ip.sh $2
+		update-nova-config-ip compute $2
 		bash $(dirname $0)/manage-services.sh all restart
 		;;
 	networknode)
@@ -28,7 +58,7 @@ case $node_type in
 		;;
 	controller_networknode)
 		bash $(dirname $0)/update-etc-hosts.sh $1
-		bash $(dirname $0)/update-nova-config-ip.sh
+		update-nova-config-ip controller
 		bash $(dirname $0)/manage-services.sh all restart
 		;;
 	*)
