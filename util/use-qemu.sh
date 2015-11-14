a9239e9f7a5aff9ec3b82166837080e8e0da5fb2
Sriram S <sriram@innervoice.in>
2015-11-14 22:06:54 +0530

    Checking for VT-X for using QEMU

--- util/use-qemu.sh
+++ util/use-qemu.sh
@@ -3,9 +3,14 @@ echo "Node Type detected as: $node_type"
 
 if [ "$node_type" == "compute" ] || [ "$node_type" == "allinone" ]
 	then
-		crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
-		sleep 2
-		service nova-compute restart
+		egrep ‘(vmx|svm)’ /proc/cpuinfo
+		if [ $? -eq 1 ]
+			then
+				echo "Virtualization Extensions not available - using QEMU" 
+				crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
+				sleep 2
+				service nova-compute restart
+		fi
 else
 	echo "This command does not work for Node type: $node_type"
 fi
