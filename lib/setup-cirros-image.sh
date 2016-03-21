a0988074d020f99cbc35bd12e9b17af2a6a52626
Sriram S <sriram@innervoice.in>
2016-03-21 14:08:38 +0530

    Updated Cirros/Glance image create command

--- lib/setup-cirros-image.sh
+++ lib/setup-cirros-image.sh
@@ -1,6 +1,6 @@
 source $(dirname $0)/config-parameters.sh
 
-glance_image_name="cirros-0.3.4-x86_64"
+glance_image_name="cirros"
 if [ ! -z $1 ]
 	then
 		glance_image_name=$1
@@ -13,7 +13,7 @@ echo_and_sleep "Cirros Image URL: $wget_url_for_cirros"
 wget $wget_url_for_cirros
 source $(dirname $0)/admin_openrc.sh
 echo_and_sleep "Downloaded Cirros Image. Ready to call glance image-create command" 3
-glance image-create --name $glance_image_name --file $cirros_image_name --disk-format qcow2 --container-format bare --is-public True --progress
+openstack image create --public --file $cirros_image_name --disk-format qcow2 --container-format bare $glance_image_name
 echo_and_sleep "Completed glance image-create"
 glance image-list
 echo_and_sleep "Verify Glance Image List" 7
