b965ef55cf6259a89cb03e6dacda112b4efa9c50
Sriram S <sriram@innervoice.in>
2016-04-14 23:19:57 +0530

    Updated config script for Horizon

--- lib/configure-horizon.sh
+++ lib/configure-horizon.sh
@@ -5,25 +5,13 @@ if [ $# -lt 1 ]
                 echo "Correct Syntax: $0 <cotroller-host-name>"
                 exit 1
 fi
+echo_and_sleep "Copying local_settings.py to /etc/openstack-dashboard"
+cp $(dirname $0)/local_settings.py /etc/openstack-dashboard/
+echo_and_sleep "Copied local_settings.py to /etc/openstack-dashboard"
 
-echo "Edit /etc/openstack-dashboard/local_settings.py as follows:"
-echo "*********"
-echo "OPENSTACK_HOST = \"controller\"" 
-echo "ALLOWED_HOSTS = ['*']" 
-echo "CACHES = {" 
-echo "    'default': {" 
-echo "        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache'," 
-echo "        'LOCATION': '127.0.0.1:11211'," 
-echo "    }" 
-echo "}" 
-echo "*********"
-sed -e "/^ALLOWED_HOSTS =.*$/s/^.*$/ALLOWED_HOSTS = ['*']/" -i /etc/openstack-dashboard/local_settings.py
 sed -e "/^OPENSTACK_HOST =.*$/s/^.*$/OPENSTACK_HOST = \""$1"\"/" -i /etc/openstack-dashboard/local_settings.py
-sed -e "/^OPENSTACK_KEYSTONE_DEFAULT_ROLE =.*$/s/^.*$/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"user\"/" -i /etc/openstack-dashboard/local_settings.py
-sed -e "/^OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT =.*$/s/^.*$/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = \"True\"/" -i /etc/openstack-dashboard/local_settings.py
-grep "ALLOWED_HOSTS" /etc/openstack-dashboard/local_settings.py
+sed -e "/^'LOCATION.*$/s/^.*$/'LOCATION': \'"$1:1121"\'/" -i /etc/openstack-dashboard/local_settings.py
 grep "OPENSTACK_HOST" /etc/openstack-dashboard/local_settings.py
-echo_and_sleep "Restarting apache2" 3
+grep "LOCATION" /etc/openstack-dashboard/local_settings.py
+echo_and_sleep "Restarting apache2" 5
 service apache2 reload
-echo_and_sleep "Restarting memcached" 3
-service memcached restart
