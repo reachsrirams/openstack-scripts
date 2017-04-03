643a5c7f7b922cd25c087f4b2ab44364ad5a3954
Sriram S <sriram@innervoice.in>
2017-04-03 12:34:22 +0530

    Removing references to controller wherever possible

--- lib/admin_openrc.sh
+++ lib/admin_openrc.sh
@@ -1,8 +1,11 @@
+echo "Running: $0 $@"
+source $(dirname $0)/config-parameters.sh
+
 export OS_PROJECT_DOMAIN_NAME=default
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=password
-export OS_AUTH_URL=http://controller:35357/v3
+export OS_AUTH_URL=http://$controller_host_name:35357/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
