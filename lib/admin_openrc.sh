72153bf47a7452ed4cf282e06d46f8ba8d8ba60e
Sriram S <sriram@innervoice.in>
2017-04-04 14:25:15 +0530

    Removing references to controller from RC file

--- lib/admin_openrc.sh
+++ lib/admin_openrc.sh
@@ -1,11 +1,24 @@
-echo "Running: $0 $@"
-source $(dirname $0)/config-parameters.sh
+if [ "$controller_host_name" == "" ]
+then
+	echo "Controller Host Name Environment variable is blank. Checking command line argument."
+	if [ "$1" == "" ]
+	then
+		echo "Controller Host Name Command line argument is blank. Using default name for Controller."
+		final_controller_host_name="controller"
+	else
+		final_controller_host_name=$1
+	fi	
+else
+	final_controller_host_name=$controller_host_name
+fi
 
+echo "Final Controller Host Name: "$final_controller_host_name
+	
 export OS_PROJECT_DOMAIN_NAME=default
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=password
-export OS_AUTH_URL=http://$controller_host_name:35357/v3
+export OS_AUTH_URL=http://$final_controller_host_name:35357/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
