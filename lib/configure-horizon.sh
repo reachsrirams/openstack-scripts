068a6279947bc4018e0def8eaeb338b0a319459e
Sriram S <sriram@innervoice.in>
2017-04-13 12:33:35 +0530

    Optimized sleep time wherever applicable

--- lib/configure-horizon.sh
+++ lib/configure-horizon.sh
@@ -5,13 +5,14 @@ if [ $# -lt 1 ]
                 echo "Correct Syntax: $0 <cotroller-host-name>"
                 exit 1
 fi
-echo_and_sleep "Copying local_settings.py to /etc/openstack-dashboard"
+echo_and_sleep "Copying local_settings.py to /etc/openstack-dashboard" 2
 cp $(dirname $0)/local_settings.py /etc/openstack-dashboard/
-echo_and_sleep "Copied local_settings.py to /etc/openstack-dashboard"
+echo_and_sleep "Copied local_settings.py to /etc/openstack-dashboard" 2
 
 sed -e "/^OPENSTACK_HOST =.*$/s/^.*$/OPENSTACK_HOST = \""$1"\"/" -i /etc/openstack-dashboard/local_settings.py
 sed -e "/^'LOCATION.*$/s/^.*$/'LOCATION': \'"$1:1121"\'/" -i /etc/openstack-dashboard/local_settings.py
 grep "OPENSTACK_HOST" /etc/openstack-dashboard/local_settings.py
 grep "LOCATION" /etc/openstack-dashboard/local_settings.py
-echo_and_sleep "Restarting apache2" 5
+echo_and_sleep "Restarting apache2" 2
 service apache2 reload
+echo_and_sleep "Restarted apache2" 2
